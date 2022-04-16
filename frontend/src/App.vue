88ac078ed06032e4e71adfc581e3f91300bfeacc
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-16 08:11:36 -0300

    Diretiva v-bind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <h1>Hello</h1>
+  <a v-bind:href="product.url" v-bind:class="productClass">{{ product.name }} </a>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data() {
+    return {
+      product: {
+        name: 'Camisa',
+        url: 'http://loja.com/produtos/123456',
+        stock: true
+      }
+    }
+  },
+  computed: {
+    productClass(): string {
+      return this.product.stock ? 'sucess' : 'danger'
+    }
+  }
+})
 </script>
 
-<style></style>
+<style>
+.danger {
+  color: red;
+}
+
+.sucess {
+  color: green;
+}
+</style>
