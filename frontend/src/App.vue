8dec0daba88b7e378cec22428a3ec89b950109fe
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-16 13:31:38 -0300

    Diretiva v-model com objeto

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <form>
-    <input type="text" placeholder="Nome" v-model.lazy="name" />
+    <input type="text" placeholder="Nome" v-model.lazy="form.name" />
 
-    <input type="number" placeholder="Idade" v-model.number="age" />
+    <input type="number" placeholder="Idade" v-model.number="form.age" />
 
-    <textarea rows="2" placeholder="Descrição" v-model.trim="message"></textarea>
+    <textarea rows="2" placeholder="Descrição" v-model.trim="form.message"></textarea>
 
-    <input type="checkbox" v-model="newsletter" /> Newsletter <br />
+    <input type="checkbox" v-model="form.newsletter" /> Newsletter <br />
 
-    <input type="radio" name="color" value="vermelho" v-model="color" /> Vermelho
-    <input type="radio" name="color" value="verde" v-model="color" /> Verde
-    <input type="radio" name="color" value="azul" v-model="color" /> Azul
+    <input type="radio" name="color" value="vermelho" v-model="form.color" /> Vermelho
+    <input type="radio" name="color" value="verde" v-model="form.color" /> Verde
+    <input type="radio" name="color" value="azul" v-model="form.color" /> Azul
 
-    <select v-model="fruit">
+    <select v-model="form.fruit">
       <option disabled selected>Selecione uma fruta</option>
       <option value="uva">Uva</option>
       <option value="banana">Banana</option>
@@ -20,12 +20,12 @@
     </select>
   </form>
 
-  <p>Nome: {{ name }}</p>
-  <p>Idade: {{ age }}</p>
-  <p>Mensagem: {{ message }}</p>
-  <p>Newsletter: {{ newsletter }}</p>
-  <p>Cor: {{ color }}</p>
-  <p>Fruta: {{ fruit }}</p>
+  <p>Nome: {{ form.name }}</p>
+  <p>Idade: {{ form.age }}</p>
+  <p>Mensagem: {{ form.message }}</p>
+  <p>Newsletter: {{ form.newsletter }}</p>
+  <p>Cor: {{ form.color }}</p>
+  <p>Fruta: {{ form.fruit }}</p>
 </template>
 
 <script lang="ts">
@@ -34,12 +34,19 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      name: '',
-      age: 0,
-      message: '',
-      newsletter: false,
-      color: '',
-      fruit: ''
+      form: {
+        name: '',
+        age: 0,
+        message: '',
+        newsletter: false,
+        color: '',
+        fruit: ''
+      }
+    }
+  },
+  methods: {
+    send() {
+      // axios.post('/register', this.form)
     }
   }
 })
