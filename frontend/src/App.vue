59d7c386e4747f30d644389ab2f8e31f8cd00e3d
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-16 13:00:12 -0300

    Diretiva v-model e modificadores

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <ul>
-    <li v-for="product in products" v-bind:key="product.id">
-      {{ product.name }}
-    </li>
-  </ul>
+  <form>
+    <input type="text" placeholder="Nome" v-model.lazy="name" />
+
+    <input type="number" placeholder="Idade" v-model.number="age" />
+
+    <textarea rows="2" placeholder="Descrição" v-model.trim="message"></textarea>
+
+    <input type="checkbox" v-model="newsletter" /> Newsletter <br />
+
+    <input type="radio" name="color" value="vermelho" v-model="color" /> Vermelho
+    <input type="radio" name="color" value="verde" v-model="color" /> Verde
+    <input type="radio" name="color" value="azul" v-model="color" /> Azul
+
+    <select v-model="fruit">
+      <option disabled selected>Selecione uma fruta</option>
+      <option value="uva">Uva</option>
+      <option value="banana">Banana</option>
+      <option value="morango">Morango</option>
+    </select>
+  </form>
+
+  <p>Nome: {{ name }}</p>
+  <p>Idade: {{ age }}</p>
+  <p>Mensagem: {{ message }}</p>
+  <p>Newsletter: {{ newsletter }}</p>
+  <p>Cor: {{ color }}</p>
+  <p>Fruta: {{ fruit }}</p>
 </template>
 
 <script lang="ts">
@@ -12,13 +34,27 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      products: [
-        { id: 1, name: 'Camisa', stock: true },
-        { id: 2, name: 'Bota', stock: false },
-        { id: 3, name: 'Chapéu', stock: false },
-        { id: 4, name: 'Cinto', stock: true }
-      ]
+      name: '',
+      age: 0,
+      message: '',
+      newsletter: false,
+      color: '',
+      fruit: ''
     }
   }
 })
 </script>
+
+<style scoped>
+input[type='email'],
+input[type='text'],
+select,
+textarea {
+  display: block;
+}
+
+form {
+  padding-bottom: 1rem;
+  border-bottom: 2px solid #000;
+}
+</style>
