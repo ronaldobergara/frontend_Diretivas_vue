5d12ca1f4e3d027e10946a3174f918393f307980
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-16 23:30:16 -0300

    Classes dinamicas, sintaxe de objeto

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <h1 v-highlight:background.shadow="'red'">Hello Kaori</h1>
+  <h1 class="static" :class="{ active: isActive, 'text-danger': hasError }">Hello Kaori</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data() {
+    return {
+      isActive: true,
+      hasError: true
+    }
+  }
+})
 </script>
