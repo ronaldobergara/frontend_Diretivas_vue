a19cc3867432cb7940704fd290e5c4f50c5c6c42
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-22 14:00:23 -0300

    Componente Dinamico

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <ButtonStyled text="Botão" />
+  <ul>
+    <li @click="currentView = 'Home'">Home</li>
+    <li @click="currentView = 'About'">Sobre</li>
+    <li @click="currentView = 'Contact'">Contato</li>
+  </ul>
+
+  <component :is="currentView" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import Contact from '@/pages/Contact.vue'
 
-export default defineComponent({})
+export default defineComponent({
+  components: { Home, About, Contact },
+  data() {
+    return {
+      currentView: 'Home'
+    }
+  }
+})
 </script>
 
 <style scoped></style>
