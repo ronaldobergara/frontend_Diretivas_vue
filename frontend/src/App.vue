0ca65d8d7ca05dce8dbaf62a52165a6f304f9e38
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-23 00:47:41 -0300

    Transições Custom

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <ButtonStyled @click="show = !show" text="Toggle" />
 
-  <transition name="fade">
+  <transition
+    enter-active-class="animate__animated animate__backInDown"
+    leave-active-class="animate__animated animate__backOutDown"
+  >
     <h1 v-if="show">Kaori</h1>
-  </transition>
-
-  <transition name="fade2">
-    <h1 v-if="show">Bergara</h1>
   </transition>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import ButtonStyled from './components/ButtonStyled.vue'
+import 'animate.css'
 
 export default defineComponent({
   data() {
@@ -23,36 +22,4 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-/*
-v-enter-from (de onde estar vindo)
-v-enter-active (como vai acontecer)
-v-enter-to (para onde está indo)
-
-v-leave-from (de onde estar vindo)
-v-leave-active (como vai acontecer)
-v-leave-to (para onde está indo)
-
-O nome inicial `v` depende do atributo `name`
-*/
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-
-.fade2-enter-from,
-.fade2-leave-to {
-  opacity: 0;
-}
-
-.fade2-enter-active,
-.fade2-leave-active {
-  transition: opacity 0.9s;
-}
-</style>
+<style scoped></style>
