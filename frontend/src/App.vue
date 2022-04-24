db6aa901b488b73ed40d800b18ec671d64dd4221
ronaldobergara <bergaraheadbanger@gmail.com>
2022-04-24 01:28:22 -0300

    Transição list

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,44 @@
 <template>
-  <ul>
-    <li @click="view = 'Home'">Home</li>
-    <li @click="view = 'Contact'">Contatos</li>
-    <li @click="view = 'About'">Sobre</li>
-  </ul>
+  <ButtonStyled @click="filterAZ" text="Filtro AZ" />
+  <ButtonStyled @click="filterZA" text="Filtro ZA" />
 
-  <transition
+  <transition-group
     enter-active-class="animate__animated animate__fadeIn"
     leave-active-class="animate__animated animate__fadeOut"
     :appear="true"
     mode="out-in"
   >
-    <component :is="view" />
-  </transition>
+    <!-- <component :is="view" /> -->
+
+    <li v-for="user in users" :key="user">
+      {{ user }}
+    </li>
+  </transition-group>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import 'animate.css'
-import Home from '@/pages/Home.vue'
-import Contact from '@/pages/Contact.vue'
-import About from '@/pages/About.vue'
 
 export default defineComponent({
-  components: { Home, Contact, About },
   data() {
     return {
-      view: 'Home'
+      users: ['Yung', 'João', 'Kaori', 'Blondie', 'Miguel']
+    }
+  },
+  methods: {
+    filterAZ() {
+      this.users.sort()
+    },
+    filterZA() {
+      this.users.sort().reverse()
     }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-move {
+  transition: transform 0.5s;
+}
+</style>
